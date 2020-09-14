2d486fefa8af61e8e879953f0393dec55f37d550
rentamaeda <japan_lunch_fate@yahoo.co.jp>
2020-09-14 17:33:30 +0900

    完成

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatPage extends StatefulWidget {
-
   ChatPage(this._userName);
+
   final String _userName;
 
   @override
@@ -10,8 +12,11 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    Firebase.initializeApp();
     return new Scaffold(
         appBar: new AppBar(
           title: new Text("チャットページ"),
@@ -20,12 +25,33 @@ class _ChatPageState extends State<ChatPage> {
           margin: const EdgeInsets.symmetric(horizontal: 8.0),
           child: Column(
             children: <Widget>[
-              new Flexible(
-                child: new ListView.builder(
-                  padding: new EdgeInsets.all(8.0),
-                  reverse: true,
-                  itemBuilder: (_, int index){},
-                  itemCount: 10,
+              Flexible(
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: Firestore.instance
+                      .collection("chat_room")
+                      .orderBy("created_at", descending: true)
+                      .snapshots() ,
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) return Container();
+                    return new ListView.builder(
+                      padding: new EdgeInsets.all(8.0),
+                      reverse: true,
+                      itemBuilder: (_, int index) {
+                        DocumentSnapshot document =
+                        snapshot.data.documents[index]  ;
+                        bool isOwnMessage = false;
+                        if (document.data()['user_name'] == widget._userName) {
+                          isOwnMessage = true ;
+                        }
+                        return isOwnMessage
+                            ? _ownMessage(
+                            document.data()['message'], document.data()['user_name'])
+                            : _message(
+                            document.data()['message'], document.data()['user_name']);
+                      },
+                      itemCount: snapshot.data.documents.length,
+                    );
+                  },
                 ),
               ),
               new Divider(height: 1.0),
@@ -35,21 +61,74 @@ class _ChatPageState extends State<ChatPage> {
                   children: <Widget>[
                     new Flexible(
                       child: new TextField(
-                        decoration: new InputDecoration.collapsed(
-                            hintText: "メッセージの送信"),
+                        controller: _controller,
+                        onSubmitted: _handleSubmit,
+                        decoration:
+                        new InputDecoration.collapsed(hintText: "メッセージの送信"),
                       ),
                     ),
                     new Container(
                       child: new IconButton(
-                          icon: new Icon(Icons.send, color: Colors.blue,),
-                          onPressed: () {}),
+                          icon: new Icon(
+                            Icons.send,
+                            color: Colors.blue,
+                          ),
+                          onPressed: () {
+                            _handleSubmit(_controller.text);
+                          }),
                     ),
                   ],
                 ),
               ),
             ],
           ),
-        )
+        ));
+  }
+
+  Widget _ownMessage(String message, String userName) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 10.0,),
+            Text(userName),
+            Text(message),
+          ],
+        ),
+        Icon(Icons.person),
+      ],
     );
+  }
+
+  Widget _message(String message, String userName) {
+    return Row(
+      children: <Widget>[
+        Icon(Icons.person),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 10.0,),
+            Text(userName),
+            Text(message),
+          ],
+        )
+      ],
+    );
+  }
+
+  _handleSubmit(String message) {
+    _controller.text = "";
+    var db = Firestore.instance;
+    db.collection("chat_room").add({
+      "user_name": widget._userName,
+      "message": message,
+      "created_at": DateTime.now()
+    }).then((val) {
+      print("成功です");
+    }).catchError((err) {
+      print(err);
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,30 @@
 import 'package:chat_app/chat.dart';
 import 'package:flutter/material.dart';
-void main() {
-  runApp(MyApp());
-}
+
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      theme: new ThemeData(
+          primaryColor: Colors.white
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _controller = TextEditingController();
 
+  final _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -45,6 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Container(
             margin: EdgeInsets.all(10.0),
             child: TextFormField(
+              controller: _controller,
               decoration: InputDecoration(
                   labelText: 'ユーザー名'
               ),
@@ -52,9 +45,9 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           RaisedButton(
             color: Colors.blue,
-            onPressed: (){
+            onPressed: () {
               Navigator.push(
-                context, MaterialPageRoute(builder: (builder) => ChatPage(_controller.text)));
+                  context, MaterialPageRoute(builder: (builder) => ChatPage(_controller.text)));
             },
             child: Text("設定完了", style: TextStyle(color: Colors.white),),
           )
